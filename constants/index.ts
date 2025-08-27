bb680f66b44f8ccc275733c95bf752265446721b
David <davidwen25@gmail.com>
2025-08-27 01:00:59 -0700

    Added Icons/Images and create constants file, also created images.d.ts for imports

---
+++ constants/index.ts
@@ -0,0 +1,21 @@
+import add from "../assets/icons/addBtn.png";
+import addWork from "../assets/icons/addWorkoutBtn.png";
+import calendar from "../assets/icons/calendarBtn.png";
+import data from "../assets/icons/dataIcon.png";
+import edit from "../assets/icons/editIcon.png";
+import home from "../assets/icons/homeIcon.png";
+import ladder from "../assets/icons/ladder.png";
+import reload from "../assets/icons/reload.png";
+import timer from "../assets/icons/timerIcon.png";
+
+export const images = {
+    add,
+    addWork,
+    calendar,
+    data,
+    edit,
+    home,
+    ladder,
+    reload,
+    timer,
+}

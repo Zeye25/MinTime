b26a80b084d5b97bf3d0c33915a03bea2d0d8cf2
David <davidwen25@gmail.com>
2025-08-26 22:52:54 -0700

    Added essential colors needed

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,13 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-xl font-bold text-primary-100">
+        Welcome to Nativewind!
+      </Text>
+      <Text className="text-xl font-bold text-primary-200">
+        Welcome to Nativewind!
+      </Text>
+      <Text className="text-xl font-bold text-primary-300">
         Welcome to Nativewind!
       </Text>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-
+        primary: {
+          100: '#FF6F00',
+          200: '#FF6F00E6',
+          300: '#FF6F00B3',
+        },
+        secondary: '#FFA726',
+        white: {
+          default: '#FFFFFF',
+          text: '#F5F5F5',
+        },
+        dark: {
+          100: '#121212',
+          200: '#000000',
+        },
+        grey: {
+          100: '#414141',
+        },
+        red: {
+          100: '#FF0000'
+        },
       },
+      
     },
   },
   plugins: [],

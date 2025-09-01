328df5fe4092b66c6c8eae598da807830330af1c
David <davidwen25@gmail.com>
2025-08-31 23:11:56 -0700

    Added tabs navigation barebones

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,26 @@
+import { Tabs } from "expo-router";
+
 export default function TabLayout() {
-    
+    return (
+        <Tabs>
+            <Tabs.Screen 
+                name='timer'
+                options={{
+                    title: 'Timer',
+                }}
+            />
+            <Tabs.Screen 
+                name='index'
+                options={{
+                    title: 'Home',
+                }}
+            />
+            <Tabs.Screen 
+                name='data'
+                options={{
+                    title: 'Data',
+                }}
+            />
+        </Tabs>
+    );
 }
